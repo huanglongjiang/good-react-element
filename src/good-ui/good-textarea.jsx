1ea74b4dc83a6be1287cdcc18977bd42a613e588
testUser <testEmail@qq.com>
2020-08-06 23:36:46 +0800

    清空数据

--- src/good-ui/good-textarea.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default class Buton extends React.Component {
-  render() {
-
-    return (
-	    <textarea maxlength="200" class="input-default height-150 width-400" style={{'background':'#fff'}}></textarea>
-    );
-  }
-}
